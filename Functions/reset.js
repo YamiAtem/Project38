64d6af1527b06bb355c0fa9b30a4c4194c0d709c
Atharva Mishra <atharvamishra956@gmail.com>
2020-12-02 19:40:58 +0530

    added reset to keep reset function

---
+++ Functions/reset.js
@@ -0,0 +1,18 @@
+function reset(){
+    gameState = PLAY;
+    gameOver.visible = false;
+    restart.visible = false;
+    
+    obstaclesGroup.destroyEach();
+    cloudsGroup.destroyEach();
+    
+    trex.changeAnimation("running",trex_running);
+    
+    if(localStorage["HighestScore"] < score){
+        localStorage["HighestScore"] = score;
+    }
+    console.log(localStorage["HighestScore"]);
+    
+    score = 0;
+    
+}
